bcaa24afa2202a1ca43b1e23e24ff83af415d376
Rnunfer <rnunfer072b@g.educaand.es>
2022-12-21 13:26:28 +0100

    tarea 3.7

--- src/main/java/org/iesvdm/dto/ComercialDTO.java
+++ src/main/java/org/iesvdm/dto/ComercialDTO.java
@@ -1,16 +1,31 @@
 package org.iesvdm.dto;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import org.iesvdm.modelo.Comercial;
+import org.iesvdm.modelo.Pedido;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import static java.util.stream.Collectors.*;
+import static java.util.Comparator.*;
 
 @Data
 @AllArgsConstructor
 @SuppressWarnings("unused")
 public class ComercialDTO {
+	
+	private static final DecimalFormat df = new DecimalFormat("0.00");
+	
+	private int id;
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private float comision;
+	private List<PedidoDTO> listaPedidosDTO;
+	private static int max = 500;
 	
 	public ComercialDTO() {
 		
@@ -24,11 +39,64 @@ public class ComercialDTO {
 		this.comision = comercial.getComision();
 	}
 	
-	private int id;
-	private String nombre;
-	private String apellido1;
-	private String apellido2;
-	private float comision;
-	private List<PedidoDTO> listaPedidosDTO;
+	public String listaPedidosTotal() {
+		
+		double total = 0;
+		if (listaPedidosDTO.size() != 0) {
+			for (PedidoDTO p : listaPedidosDTO) {
+				total += p.getTotal();
+			}
+		}
+		return df.format(total);
+	}
+	
+	public String listaPedidosMedia() {
+		
+		double media = 0;
+		double total = 0;
+		if (listaPedidosDTO.size() != 0) {
+			for (PedidoDTO p : listaPedidosDTO) {
+				total += p.getTotal();
+			}
+			media = total / listaPedidosDTO.size();
+		}
+		return df.format(media);
+	}
+
+	public double max() {
+		
+		double max = 0;
+		if (listaPedidosDTO.size() != 0) {
+			max = listaPedidosDTO.get(0).getTotal();
+			for (PedidoDTO p : listaPedidosDTO) {
+				if (p.getTotal() > max)
+					max = p.getTotal();
+			}
+		}
+		return max;
+		
+	}
+	
+	public double min() {
+		
+		double min = 0;
+		if (listaPedidosDTO.size() != 0) {
+			min = listaPedidosDTO.get(0).getTotal();
+			for (PedidoDTO p : listaPedidosDTO) {
+				if (p.getTotal() < min)
+					min = p.getTotal();
+			}
+		}
+		return min;
+		
+	}
+	
+	public List<PedidoDTO> listaOrdenada() {
+		
+		return listaPedidosDTO.stream()
+				.sorted(comparing(PedidoDTO::getTotal, reverseOrder()))
+				.collect(toList());
+	}
+	
 	
 }
